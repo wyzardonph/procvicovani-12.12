ccc585c0643e033dd8a36bbb520ba50a3808a224
růďa <145440554+wyzardonph@users.noreply.github.com>
2023-12-12 13:13:31 +0100

    Add files via upload

---
+++ 11/y.js
@@ -0,0 +1,10 @@
+for (let i = 25; i < 251; i++) {
+  if (i === 3) {
+    continue;
+  } 
+  if (i === 7) {
+    break;
+  }
+  
+  console.log(i);
+  }
---
+++ 44/y.js
@@ -0,0 +1,12 @@
+
+
+for (let i = 1; i < 0; i++) {
+    if (i === 3) {
+      continue;
+    } 
+    if (i === 7) {
+      break;
+    }
+    
+    console.log(i);
+    }
